a3a141cb9dd5d53ad38698d7543d9228fd0b4ced
dmario24 <data.mario24@gmail.com>
2025-10-01 13:29:53 +0900

    api hello -> msg, ts, kts

---
+++ src/main/java/com/example/spring_rest_api/SimpleController.java
@@ -0,0 +1,21 @@
+package com.example.spring_rest_api;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@RestController
+public class SimpleController {
+
+	@GetMapping("/hello")
+	public Map<String, Object> sayHello(){
+		
+		return Map.of(
+				"message", "Hello, World!",
+				"timesptamp", System.currentTimeMillis(),
+				"koreatime", ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toString() 
+			     );
+	}
+}
